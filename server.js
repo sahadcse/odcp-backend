efbe62aa1ff35e63d4dec33019784969c1d7a5bc
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2024-12-09 21:54:11 +0600

    Adding the Start Script on Package.json file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const socketIo = require("socket.io");
-const http = require("http"); 
+const http = require("http");
 
 // Initialize express
 const app = express();
@@ -38,6 +38,15 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('ice-candidate', candidate); // Exchange ICE candidates
   });
 
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// On server.js (or wherever your Socket.IO server is set up)
+io.on('connection', (socket) => {
+  console.log("A user connected for consultation");
+
   // End Call
   socket.on('end-call', () => {
     socket.broadcast.emit('end-call');
@@ -57,36 +66,9 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    console.log("A user disconnected");
   });
 });
-
-// On server.js (or wherever your Socket.IO server is set up)
-// io.on('connection', (socket) => {
-//   console.log("A user connected for consultation");
-
-//   // End Call
-//   socket.on('end-call', () => {
-//     socket.broadcast.emit('end-call');
-//     console.log("Call ended");
-//   });
-
-//   // Mute/Unmute
-//   socket.on('toggle-mute', (isMuted) => {
-//     socket.broadcast.emit('toggle-mute', isMuted);
-//     console.log(`Mute status: ${isMuted}`);
-//   });
-
-//   // Toggle Camera
-//   socket.on('toggle-camera', (isCameraOn) => {
-//     socket.broadcast.emit('toggle-camera', isCameraOn);
-//     console.log(`Camera status: ${isCameraOn}`);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log("A user disconnected");
-//   });
-// });
 
 
 
@@ -107,7 +89,7 @@ app.get("/", (req, res) => res.send("API Running"));
 
 
 // Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5555;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Export server for testing

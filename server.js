bb32abe6877332c3709b31de1a0f5d771a478583
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2024-12-15 00:43:38 +0600

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const socketIo = require("socket.io");
 const http = require("http");
 

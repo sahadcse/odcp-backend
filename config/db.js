918d2ebf3c5a4e1fb345b520d3b5c65b21b746b1
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2024-12-15 00:42:56 +0600

    db

--- config/db.js
+++ config/db.js
@@ -2,20 +2,26 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
-console.log("mongoDB: ", uri)
+
+if (!uri) {
+    console.error("MONGO_URI is not defined in the .env file");
+    process.exit(1);
+}
+
+console.log("mongoDB: ", uri);
 
 const connectDB = async () => {
     try {
         await mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
-    
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
-    }
+}
 
 module.exports = connectDB;
+

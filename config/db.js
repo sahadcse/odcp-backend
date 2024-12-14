06d2513eb762ffa5e46bbb4f6452c2017c384c31
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2024-12-15 00:20:21 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 
-const db = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
-        // useNewUrlParser: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
-        // useUnifiedTopology: true,
+        await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         });
     
         console.log('MongoDB Connected...');
